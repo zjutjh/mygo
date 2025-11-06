4fc79ca0c60a2737d98236c6070a1677a88e1b50
ysss-ee <xnmym_2020@qq.com>
2025-11-06 22:57:42 +0800

    feat(miniProgram):删除多余方法和配置，增加注释指路官方文档

--- wechat/miniprogram/config.go
+++ wechat/miniprogram/config.go
@@ -5,21 +5,17 @@ import (
 )
 
 var DefaultConfig = Config{
-	AppId:               "",
-	AppSecret:           "",
-	Token:               "",
-	AesKey:              "",
-	RedisInstance:       "",
-	AccessTokenCacheKey: "",
-
-	Timeout: 5 * time.Second,
+	AppId:     "",
+	AppSecret: "",
+	Token:     "",
+	AesKey:    "",
 
 	HttpDebug: true,
 
 	Log: LogConfig{
 		Level:  "debug",
-		File:   "/Users/User/wechat/mini-program/info.log",
-		Error:  "/Users/User/wechat/mini-program/error.log",
+		File:   "",
+		Error:  "",
 		Stdout: false,
 	},
 
@@ -27,32 +23,14 @@ var DefaultConfig = Config{
 	MaxActive:   10,
 	MaxIdle:     10,
 	IdleTimeout: 60 * time.Second,
-
-	TLSHandshakeTimeout:    0,
-	DisableKeepAlives:      false,
-	DisableCompression:     false,
-	MaxIdleConns:           0,
-	MaxIdleConnsPerHost:    200,
-	MaxConnsPerHost:        500,
-	IdleConnTimeout:        30 * time.Second,
-	ResponseHeaderTimeout:  0,
-	ExpectContinueTimeout:  1 * time.Second,
-	MaxResponseHeaderBytes: 0,
-	WriteBufferSize:        0,
-	ReadBufferSize:         0,
-	ForceAttemptHTTP2:      true,
-	DialContextTimeout:     30 * time.Second,
-	DialContextKeepAlive:   30 * time.Second,
 }
 
 type Config struct {
 	//小程序配置
-	AppId               string `mapstructure:"app_id"`
-	AppSecret           string `mapstructure:"app_secret"`
-	Token               string `mapstructure:"token"`
-	AesKey              string `mapstructure:"aes_key"`
-	RedisInstance       string `mapstructure:"redis_"`
-	AccessTokenCacheKey string `mapstructure:"access_token_cache_key"`
+	AppId     string `mapstructure:"app_id"`
+	AppSecret string `mapstructure:"app_secret"`
+	Token     string `mapstructure:"token"`
+	AesKey    string `mapstructure:"aes_key"`
 
 	HttpDebug bool `mapstructure:"http_debug"`
 
@@ -63,24 +41,6 @@ type Config struct {
 	MaxActive   int           `mapstructure:"max_active"`
 	MaxIdle     int           `mapstructure:"max_idle"`
 	IdleTimeout time.Duration `mapstructure:"idle_timeout"`
-
-	// HTTP Client Transport
-	TLSHandshakeTimeout    time.Duration `mapstructure:"tls_handshake_timeout"`
-	DisableKeepAlives      bool          `mapstructure:"disable_keep_alives"`
-	DisableCompression     bool          `mapstructure:"disable_compression"`
-	MaxIdleConns           int           `mapstructure:"max_idle_conns"`
-	MaxIdleConnsPerHost    int           `mapstructure:"max_idle_conns_per_host"`
-	MaxConnsPerHost        int           `mapstructure:"max_conns_per_host"`
-	IdleConnTimeout        time.Duration `mapstructure:"idle_conn_timeout"`
-	ResponseHeaderTimeout  time.Duration `mapstructure:"response_header_timeout"`
-	ExpectContinueTimeout  time.Duration `mapstructure:"expect_continue_timeout"`
-	MaxResponseHeaderBytes int64         `mapstructure:"max_response_header_bytes"`
-	WriteBufferSize        int           `mapstructure:"write_buffer_size"`
-	ReadBufferSize         int           `mapstructure:"read_buffer_size"`
-	ForceAttemptHTTP2      bool          `mapstructure:"force_attempt_http2"`
-	DialContextTimeout     time.Duration `mapstructure:"dial_context_timeout"`
-	DialContextKeepAlive   time.Duration `mapstructure:"dial_context_keep_alive"`
-	Timeout                time.Duration
 }
 
 type LogConfig struct {
--- wechat/miniprogram/wechat.go
+++ wechat/miniprogram/wechat.go
@@ -1,74 +1,33 @@
 package miniprogram
 
 import (
-	"context"
-	"crypto/aes"
-	"crypto/cipher"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"net"
-	"net/http"
-	"time"
-
 	"github.com/ArtisanCloud/PowerLibs/v3/cache"
 	"github.com/ArtisanCloud/PowerWeChat/v3/src/kernel/response"
 	"github.com/ArtisanCloud/PowerWeChat/v3/src/miniProgram"
-	"github.com/go-resty/resty/v2"
 	"github.com/redis/go-redis/v9"
-	"github.com/tidwall/gjson"
-	"github.com/zjutjh/mygo/kit"
 	"github.com/zjutjh/mygo/nedis"
 )
 
-type WeChatService interface {
-	GetAccessToken() (string, error)
-	GetMiniProgram() *miniProgram.MiniProgram
-	GetConfig() *Config
-}
 type WeChat struct {
 	conf        Config
 	miniProgram *miniProgram.MiniProgram
-	client      resty.Client
 }
+
+/**
+ * 使用方法请参考官方文档
+ * 入门://powerwechat.artisan-cloud.com/zh/mini-program/
+ * 小程序登录：https://developers.weixin.qq.com/miniprogram/dev/framework/open-ability/login.html
+ * 获取用户信息：https://powerwechat.artisan-cloud.com/zh/mini-program/user-info.html
+ */
 
 // New 创建微信服务实例
 func New(conf Config) *WeChat {
-	// 初始化HTTP Client实例
-	hc := &http.Client{
-		Timeout: conf.Timeout,
-		Transport: &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   conf.DialContextTimeout,
-				KeepAlive: conf.DialContextKeepAlive,
-			}).DialContext,
-			TLSHandshakeTimeout:    conf.TLSHandshakeTimeout,
-			DisableKeepAlives:      conf.DisableKeepAlives,
-			DisableCompression:     conf.DisableCompression,
-			MaxIdleConns:           conf.MaxIdleConns,
-			MaxIdleConnsPerHost:    conf.MaxIdleConnsPerHost,
-			MaxConnsPerHost:        conf.MaxConnsPerHost,
-			IdleConnTimeout:        conf.IdleConnTimeout,
-			ResponseHeaderTimeout:  conf.ResponseHeaderTimeout,
-			ExpectContinueTimeout:  conf.ExpectContinueTimeout,
-			MaxResponseHeaderBytes: conf.MaxResponseHeaderBytes,
-			WriteBufferSize:        conf.WriteBufferSize,
-			ReadBufferSize:         conf.ReadBufferSize,
-			ForceAttemptHTTP2:      conf.ForceAttemptHTTP2,
-		},
-	}
-
-	// 初始化resty Client
-	client := resty.NewWithClient(hc)
 
 	var kernelCache cache.CacheInterface
-	// 配置缓存
 	gr := cache.NewGRedis(&redis.UniversalOptions{})
 	gr.Pool = nedis.Pick(conf.Redis)
 	kernelCache = gr
 
-	// 创建小程序实例
 	mp, _ := miniProgram.NewMiniProgram(&miniProgram.UserConfig{
 		AppID:        conf.AppId,
 		Secret:       conf.AppSecret,
@@ -84,163 +43,11 @@ func New(conf Config) *WeChat {
 			Stdout: conf.Log.Stdout,
 		},
 
-		// 应该使用AccessTokenCacheKey作为缓存键名
-
 		Cache: kernelCache,
 	})
 
 	return &WeChat{
 		conf:        conf,
 		miniProgram: mp,
-		client:      *client,
 	}
-}
-
-type Code2SessionResponse struct {
-	OpenID     string `json:"openid"`
-	SessionKey string `json:"session_key"`
-	UnionID    string `json:"unionid,omitempty"`
-	ErrCode    int    `json:"errcode,omitempty"`
-	ErrMsg     string `json:"errmsg,omitempty"`
-}
-
-// Code2Session 小程序登录身份验证
-func (wc *WeChat) Code2Session(code string) (*Code2SessionResponse, error) {
-	if code == "" {
-		return nil, fmt.Errorf("%w: code不能为空", kit.ErrRequestInvalidParamter)
-	}
-
-	resp, err := wc.client.R().SetQueryParams(map[string]string{
-		"appid":      wc.conf.AppId,
-		"secret":     wc.conf.AppSecret,
-		"js_code":    code,
-		"grant_type": "authorization_code",
-	}).Get("https://api.weixin.qq.com/sns/jscode2session")
-
-	if err != nil {
-		return nil, fmt.Errorf("小程序登录请求错误: %w", err)
-	}
-
-	if resp.StatusCode() != http.StatusOK {
-		return nil, fmt.Errorf("小程序登录状态码错误: %d", resp.StatusCode())
-	}
-
-	var result Code2SessionResponse
-	if err := json.Unmarshal(resp.Body(), &result); err != nil {
-		return nil, fmt.Errorf("解析登录响应失败: %w", err)
-	}
-
-	if result.ErrCode != 0 {
-		return nil, fmt.Errorf("小程序登录业务错误: %d - %s", result.ErrCode, result.ErrMsg)
-	}
-
-	if result.OpenID == "" {
-		return nil, fmt.Errorf("获取OpenID失败: %s", string(resp.Body()))
-	}
-
-	return &result, nil
-}
-
-// GetAccessToken 获取AccessToken
-func (wc *WeChat) GetAccessToken() (string, error) {
-
-	// Redis缓存中获取access_token
-	rdb := nedis.Pick(wc.conf.RedisInstance)
-	if rdb != nil {
-		if val, err := rdb.Get(context.Background(), wc.conf.AccessTokenCacheKey).Result(); err == nil {
-			return val, nil
-		}
-	}
-
-	// 重新获取access_token
-	resp, err := wc.client.R().
-		SetQueryParams(map[string]string{
-			"grant_type": "client_credential",
-			"appid":      wc.conf.AppId,
-			"secret":     wc.conf.AppSecret,
-		}).
-		SetHeader("Accept", "application/json").
-		Get("https://api.weixin.qq.com/cgi-bin/token")
-
-	if err != nil {
-		return "", fmt.Errorf("获取AccessToken请求错误: %w", err)
-	}
-
-	if resp.StatusCode() != http.StatusOK {
-		return "", fmt.Errorf("获取AccessToken状态码错误: %d", resp.StatusCode())
-	}
-
-	jsonData := string(resp.Body())
-	accessToken := gjson.Get(jsonData, "access_token").String()
-	if accessToken == "" {
-		return "", fmt.Errorf("获取AccessToken失败: %s", jsonData)
-	}
-
-	// 缓存access_token
-	expireTime := gjson.Get(jsonData, "expires_in").Int() - 60*30
-	if rdb != nil {
-		rdb.Set(context.Background(), wc.conf.AccessTokenCacheKey, accessToken, time.Duration(expireTime)*time.Second)
-	}
-
-	return accessToken, nil
-}
-func (wc *WeChat) GetMiniProgram() *miniProgram.MiniProgram {
-	return wc.miniProgram
-}
-func (wc *WeChat) GetConfig() *Config {
-	return &wc.conf
-}
-
-// DecryptUserData 添加用户敏感信息解密（手机号、用户详细信息等）
-func (wc *WeChat) DecryptUserData(encryptedData, iv, sessionKey string) (map[string]interface{}, error) {
-	aesKey, err := base64.StdEncoding.DecodeString(sessionKey)
-	if err != nil {
-		return nil, fmt.Errorf("sessionKey解码失败: %w", err)
-	}
-
-	ciphertext, err := base64.StdEncoding.DecodeString(encryptedData)
-	if err != nil {
-		return nil, fmt.Errorf("encryptedData解码失败: %w", err)
-	}
-
-	aesIV, err := base64.StdEncoding.DecodeString(iv)
-	if err != nil {
-		return nil, fmt.Errorf("iv解码失败: %w", err)
-	}
-
-	// AES-128-CBC解密
-	block, err := aes.NewCipher(aesKey)
-	if err != nil {
-		return nil, fmt.Errorf("创建AES密码器失败: %w", err)
-	}
-
-	if len(ciphertext) < aes.BlockSize {
-		return nil, fmt.Errorf("密文长度不足")
-	}
-
-	if len(ciphertext)%aes.BlockSize != 0 {
-		return nil, fmt.Errorf("密文长度不是块大小的倍数")
-	}
-
-	mode := cipher.NewCBCDecrypter(block, aesIV)
-	mode.CryptBlocks(ciphertext, ciphertext)
-
-	// PKCS#7解填充
-	paddingLen := int(ciphertext[len(ciphertext)-1])
-	if paddingLen > len(ciphertext) || paddingLen > aes.BlockSize {
-		return nil, fmt.Errorf("无效的填充长度")
-	}
-
-	decrypted := ciphertext[:len(ciphertext)-paddingLen]
-
-	var userData map[string]interface{}
-	if err := json.Unmarshal(decrypted, &userData); err != nil {
-		return nil, fmt.Errorf("解析解密数据失败: %w", err)
-	}
-	// 验证appid
-	if appid, ok := userData["watermark"].(map[string]interface{})["appid"].(string); !ok || appid != wc.conf.AppId {
-		return nil, fmt.Errorf("无效的appid: %s", appid)
-	}
-
-	return userData, nil
 }
