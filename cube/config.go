f7dfa01602f2d70c964fcd25b36748d20b9f3605
SugarMGP <2350745751@qq.com>
2025-12-06 02:34:36 +0800

    refactor: 复用 nesty

--- cube/config.go
+++ cube/config.go
@@ -1,32 +1,11 @@
 package cube
 
-import (
-	"fmt"
-	"time"
-)
-
 var DefaultConfig = Config{
 	BaseURL:    "",
 	APIKey:     "",
 	BucketName: "",
 
-	Timeout: 10 * time.Second,
-
-	TLSHandshakeTimeout:    0,
-	DisableKeepAlives:      false,
-	DisableCompression:     false,
-	MaxIdleConns:           0,
-	MaxIdleConnsPerHost:    200,
-	MaxConnsPerHost:        500,
-	IdleConnTimeout:        30 * time.Second,
-	ResponseHeaderTimeout:  0,
-	ExpectContinueTimeout:  1 * time.Second,
-	MaxResponseHeaderBytes: 0,
-	WriteBufferSize:        0,
-	ReadBufferSize:         0,
-	ForceAttemptHTTP2:      true,
-	DialContextTimeout:     30 * time.Second,
-	DialContextKeepAlive:   30 * time.Second,
+	Resty: "",
 }
 
 type Config struct {
@@ -34,40 +13,6 @@ type Config struct {
 	APIKey     string `mapstructure:"api_key"`     // 应用密钥
 	BucketName string `mapstructure:"bucket_name"` // 存储桶名称
 
-	Timeout time.Duration `mapstructure:"timeout"` // HTTP 请求超时时间
-
-	// HTTP Client Transport配置
-	TLSHandshakeTimeout    time.Duration `mapstructure:"tls_handshake_timeout"`
-	DisableKeepAlives      bool          `mapstructure:"disable_keep_alives"`
-	DisableCompression     bool          `mapstructure:"disable_compression"`
-	MaxIdleConns           int           `mapstructure:"max_idle_conns"`
-	MaxIdleConnsPerHost    int           `mapstructure:"max_idle_conns_per_host"`
-	MaxConnsPerHost        int           `mapstructure:"max_conns_per_host"`
-	IdleConnTimeout        time.Duration `mapstructure:"idle_conn_timeout"`
-	ResponseHeaderTimeout  time.Duration `mapstructure:"response_header_timeout"`
-	ExpectContinueTimeout  time.Duration `mapstructure:"expect_continue_timeout"`
-	MaxResponseHeaderBytes int64         `mapstructure:"max_response_header_bytes"`
-	WriteBufferSize        int           `mapstructure:"write_buffer_size"`
-	ReadBufferSize         int           `mapstructure:"read_buffer_size"`
-	ForceAttemptHTTP2      bool          `mapstructure:"force_attempt_http2"`
-	DialContextTimeout     time.Duration `mapstructure:"dial_context_timeout"`
-	DialContextKeepAlive   time.Duration `mapstructure:"dial_context_keep_alive"`
-}
-
-type CubeError struct {
-	Code int
-	Msg  string
-}
-
-// Error 实现 error 接口
-func (e *CubeError) Error() string {
-	return fmt.Sprintf("Cube 业务错误 [Code: %d]: %s", e.Code, e.Msg)
-}
-
-// NewCubeError 是 CubeError 的工厂方法
-func NewCubeError(code int, message string) *CubeError {
-	return &CubeError{
-		Code: code,
-		Msg:  message,
-	}
+	// 基础依赖组件实例配置
+	Resty string `mapstructure:"resty"`
 }
--- cube/cube.go
+++ cube/cube.go
@@ -1,11 +1,12 @@
 package cube
 
 import (
+	"context"
 	"fmt"
 	"github.com/go-resty/resty/v2"
 	"github.com/zjutjh/mygo/kit"
+	"github.com/zjutjh/mygo/nesty"
 	"io"
-	"net"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -17,7 +18,7 @@ type CubeClient struct {
 	client *resty.Client
 }
 
-// UploadResponse 定义 Cube 文件上传 API 的标准响应结构
+// UploadResponse 定义 Cube 文件上传响应体
 type UploadResponse struct {
 	Code int    `json:"code"`
 	Msg  string `json:"msg"`
@@ -26,45 +27,25 @@ type UploadResponse struct {
 	} `json:"data"`
 }
 
-// New 创建一个 CubeClient。
+// DeleteResponse 定义 Cube 文件删除响应体
+type DeleteResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+// New 创建一个 CubeClient
 func New(conf Config) *CubeClient {
-	// 初始化HTTP Client实例
-	hc := &http.Client{
-		Timeout: conf.Timeout,
-		Transport: &http.Transport{
-			Proxy: http.ProxyFromEnvironment,
-			DialContext: (&net.Dialer{
-				Timeout:   conf.DialContextTimeout,
-				KeepAlive: conf.DialContextKeepAlive,
-			}).DialContext,
-			TLSHandshakeTimeout:    conf.TLSHandshakeTimeout,
-			DisableKeepAlives:      conf.DisableKeepAlives,
-			DisableCompression:     conf.DisableCompression,
-			MaxIdleConns:           conf.MaxIdleConns,
-			MaxIdleConnsPerHost:    conf.MaxIdleConnsPerHost,
-			MaxConnsPerHost:        conf.MaxConnsPerHost,
-			IdleConnTimeout:        conf.IdleConnTimeout,
-			ResponseHeaderTimeout:  conf.ResponseHeaderTimeout,
-			ExpectContinueTimeout:  conf.ExpectContinueTimeout,
-			MaxResponseHeaderBytes: conf.MaxResponseHeaderBytes,
-			WriteBufferSize:        conf.WriteBufferSize,
-			ReadBufferSize:         conf.ReadBufferSize,
-			ForceAttemptHTTP2:      conf.ForceAttemptHTTP2,
-		},
+	return &CubeClient{
+		conf:   conf,
+		client: nesty.Pick(conf.Resty),
 	}
-
-	client := resty.NewWithClient(hc).
-		SetBaseURL(strings.TrimRight(conf.BaseURL, "/")).
-		SetHeader("Key", conf.APIKey)
-
-	return &CubeClient{conf: conf, client: client}
 }
 
 // UploadFile 上传文件到存储立方
-func (c *CubeClient) UploadFile(filename string, reader io.Reader, location string, convertWebp, useUUID bool) (string, error) {
+func (c *CubeClient) UploadFile(ctx context.Context, filename string, reader io.Reader, location string, convertWebp, useUUID bool) (*UploadResponse, error) {
 	bucketName := c.conf.BucketName
 	if bucketName == "" {
-		return "", fmt.Errorf("%w: 未指定存储桶", kit.ErrRequestInvalidParamter)
+		return nil, fmt.Errorf("%w: 未指定存储桶", kit.ErrRequestInvalidParamter)
 	}
 
 	form := map[string]string{
@@ -76,47 +57,44 @@ func (c *CubeClient) UploadFile(filename string, reader io.Reader, location stri
 
 	var result UploadResponse
 	resp, err := c.client.R().
+		SetContext(ctx).
+		SetHeader("Key", c.conf.APIKey).
 		SetFileReader("file", filename, reader).
 		SetFormData(form).
 		SetResult(&result).
-		Post("/api/upload")
+		Post(strings.TrimRight(c.conf.BaseURL, "/") + "/api/upload")
 	if err != nil {
-		return "", fmt.Errorf("上传文件请求错误: %w", err)
+		return nil, fmt.Errorf("上传文件请求错误: %w", err)
 	}
 	if resp.StatusCode() != http.StatusOK {
-		return "", fmt.Errorf("上传文件消息状态码错误: %w", err)
+		return nil, fmt.Errorf("上传文件消息状态码错误: %v", resp.StatusCode())
 	}
-	if result.Code != 200 {
-		return "", NewCubeError(result.Code, result.Msg)
-	}
-	return result.Data.ObjectKey, nil
+	return &result, nil
 }
 
 // DeleteFile 删除文件
-func (c *CubeClient) DeleteFile(objectKey string) error {
+func (c *CubeClient) DeleteFile(objectKey string) (*DeleteResponse, error) {
 	bucketName := c.conf.BucketName
 	if bucketName == "" {
-		return fmt.Errorf("%w: 未指定存储桶", kit.ErrRequestInvalidParamter)
+		return nil, fmt.Errorf("%w: 未指定存储桶", kit.ErrRequestInvalidParamter)
 	}
 
-	var result UploadResponse
+	var result DeleteResponse
 	resp, err := c.client.R().
+		SetHeader("Key", c.conf.APIKey).
 		SetQueryParams(map[string]string{
 			"bucket":     bucketName,
 			"object_key": objectKey,
 		}).
 		SetResult(&result).
-		Delete("/api/delete")
+		Delete(strings.TrimRight(c.conf.BaseURL, "/") + "/api/delete")
 	if err != nil {
-		return fmt.Errorf("删除文件请求错误: %w", err)
+		return nil, fmt.Errorf("删除文件请求错误: %w", err)
 	}
 	if resp.StatusCode() != http.StatusOK {
-		return fmt.Errorf("删除文件消息状态码错误: %w", err)
+		return nil, fmt.Errorf("删除文件消息状态码错误: %v", resp.StatusCode())
 	}
-	if result.Code != 200 {
-		return NewCubeError(result.Code, result.Msg)
-	}
-	return nil
+	return &result, nil
 }
 
 // GetFileURL 返回文件访问 URL
